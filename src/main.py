b6996f9266d38dc2bc91f96ee23f20c091f4c58b
milesmax <davidmilesgoddard@gmail.com>
2024-12-26 10:06:12 -0500

    installed jupyter to venv and created a notebook

---
+++ src/main.py
@@ -0,0 +1,7 @@
+# This python file is intended to learn and reference the basics of pandas
+
+import pandas as pd
+
+# Goal - create a data structure that can hold asset, user, and warranty information
+
+model_data_frame = pd.DataFrame([{'serial_number' : '', }])
